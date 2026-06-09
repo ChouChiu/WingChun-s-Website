fa84372eea07752bdf765adaf6b56ac62145eaf5
ChouChiu <lshengevery@gmail.com>
2026-06-09 19:06:38 +0800

    chore: bump giscus theme version for cache bust

--- src/features/blog/components/giscus-comments.tsx
+++ src/features/blog/components/giscus-comments.tsx
@@ -14,7 +14,7 @@ const config = {
   mapping: "pathname" as const,
 }
 
-const THEME_VERSION = "2"
+const THEME_VERSION = "3"
 
 function getThemeUrl(theme: "light" | "dark") {
   if (window.location.hostname === "localhost") return theme
